023501b69731ba610bfa960fa5e28e772f92285a
franzon <brfranzon@gmail.com>
2020-05-12 17:25:59 +0200

    commit

--- app.js
+++ app.js
@@ -23,7 +23,8 @@ app.use("/", express.static(path.join(__dirname, "public")));
 // connect to DB
 let db_mongoose = "mongodb://brfranzon:#Domine1227@cluster0-shard-00-00-c5jti.mongodb.net:27017,cluster0-shard-00-01-c5jti.mongodb.net:27017,cluster0-shard-00-02-c5jti.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
 
-db.connect(db_mongoose, { useNewUrlParser: true }, () => {
+db.connect(process.env.MONGODB_URI || db_mongoose,
+    { useNewUrlParser: true }, () => {
     console.log("DB Connected...")
 })
 
